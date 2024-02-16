d12292f84185be4770062ee1d00f66a1229113c2
MCavieres <maca.c.91@gmail.com>
2024-02-16 13:52:15 -0300

    le agregue estilo css a navbar y a seccion quienes somos

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector("nav");
+
+window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 100) {
+        nav.classList.add("bg-info", "shadow");
+    } else {
+        nav.classList.remove("bg-info", "shadow");
+    }
+});
